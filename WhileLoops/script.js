d544cf0b57374ea8a98e74c5d6b318e4e6cb334c
Sanesh Gobin <126343205+SaneshGobin@users.noreply.github.com>
2025-05-02 13:39:42 +0200

    Complete WhileLoops from BroCode

--- WhileLoops/script.js
+++ WhileLoops/script.js
@@ -1,22 +1,44 @@
-let username = "";
+// let username = "";
 
-while(username === "" || username === null) {
-   username = window.prompt("Please enter name: ");
-} 
+// while(username === "" || username === null) {
+//    username = window.prompt("Please enter name: ");
+// } 
 
-let message = `Hello there ${username}`;
+// let message = `Hello there ${username}`;
 
-document.body.textContent = `Hello there ${username}.`;
+// document.body.textContent = `Hello there ${username}.`;
 
 
 
 // DO WHILE LOOP
 
-let newUser;
+// let newUser;
 
-do {
-    newUser = window.prompt("Please enter name: ");
-} while (newUser === "" || newUser === null)
+// do {
+//     newUser = window.prompt("Please enter name: ");
+// } while (newUser === "" || newUser === null)
 
-message  = message + ` Another user is ${newUser}`;
-document.body.textContent = message;
+// message  = message + ` Another user is ${newUser}`;
+// document.body.textContent = message;
+
+
+
+// Example
+
+let isLoggedIn = false;
+let username;
+let password;
+
+while(!isLoggedIn) {
+    username = window.prompt("Enter your username:" );
+    password = window.prompt("Enter your password: ");
+
+    if (username === "Sanesh" && password === "abcd1234") {
+        document.body.textContent = `Welcome ${username}`;
+        console.log("Logged in")
+        isLoggedIn = true;
+    } else {
+        document.body.textContent = `Invalid credentials!`;
+        console.log("Invalid credentials");
+    }
+}
