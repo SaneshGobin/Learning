7d57116846b52d40befa76746045964e78aefe91
Sanesh Gobin <126343205+SaneshGobin@users.noreply.github.com>
2025-05-04 16:04:33 +0200

    Fix not reponding issue. The while loop caused it

--- NumberGuessingGame/script.js
+++ NumberGuessingGame/script.js
@@ -1,26 +1,34 @@
-const min = 1;
-const max = 100;
-const luckyNumber = Math.floor(Math.random() * (max - min  + 1)) + min;
-const userGuess = document.getElementById("userGuess");
-const displayNumber = document.getElementById("displayNumber");
-const displayAttempts = document.getElementById("displayAttempts");
-const displayGameOutcome = document.getElementById("displayGameOutcome");
+const min                   = 1;
+const max                   = 100;
+const luckyNumber           = Math.floor(Math.random() * (max - min  + 1)) + min;
+const userGuess             = document.getElementById("userGuess");
+const displayNumber         = document.getElementById("displayText");
+const checkBtn              = document.getElementById("checkBtn");
+const displayAttempts       = document.getElementById("displayAttempts");
+const displayGameOutcome    = document.getElementById("displayGameOutcome");
 
 
-document.body.textContent = `${luckyNumber}`;
+let attempts = 10;
+let gameRunning = true;
 
-let attempts = 3;
-let running = true;
 
-while (running) {
-    if (Number(userGuess.value) === luckyNumber) {
-        displayNumber.textContent = `${luckyNumber}`;
-        displayGameOutcome.textContent = "You win. Congratulations!";
-
-    } else {
-        attempts--;
-        if (attempts === 0) {
-            displayGameOutcome.textContent = "You lose. Start game over!";
+    checkBtn.onclick = function() {
+        if (Number(userGuess.value) === luckyNumber) {
+            attempts--;
+            displayAttempts.textContent = `Attempts Left: ${attempts}`;
+            displayNumber.textContent = `The magic number is:\t ${luckyNumber}`;
+            displayGameOutcome.textContent = "You win. Congratulations!";
+            
+    
+        } else {
+            attempts--;
+            displayAttempts.textContent = `Attempts Left: ${attempts}`;
+            if (attempts === 0) {
+                checkBtn.disabled = true;
+                displayGameOutcome.textContent = "You lose!!";
+            }
         }
     }
-}
+
+        
+       
